a61fcee2e14319bd920cb152d9fd239f91eaf1c8
Phodal Huang <h@phodal.com>
2019-12-10 22:51:13 +0800

    feat: add basic prototype

--- main.go
+++ main.go
@@ -1,9 +1,62 @@
 package main
 
 import (
-	"sixteen/cmd"
+	"errors"
+	//"sixteen/cmd"
+	"fmt"
+	"github.com/manifoldco/promptui"
+	"strconv"
 )
 
 func main() {
-	cmd.Execute()
+	prompt := promptui.Select{
+		Label: "Refactoring",
+		Items: []string{
+			"list",
+			"step",
+			"switch",
+			"delete",
+			"create",
+		},
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Println(result)
+	switch result {
+	case "list":
+		fmt.Println("list")
+	default:
+		//listLog
+		validate()
+	}
+}
+
+func validate() {
+	validate := func(input string) error {
+		_, err := strconv.ParseFloat(input, 64)
+		if err != nil {
+			return errors.New("Invalid number")
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    "Number",
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", result)
 }
